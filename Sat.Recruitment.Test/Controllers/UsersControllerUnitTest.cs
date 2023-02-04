53987adb5b0f255c497f3c6a862dd84d011701b4
Elena <elena.sanchez.p@outlook.com>
2023-02-04 15:20:01 +0100

    Adds constructor to Unit test class.

--- Sat.Recruitment.Test/Controllers/UsersControllerUnitTest.cs
+++ Sat.Recruitment.Test/Controllers/UsersControllerUnitTest.cs
@@ -8,12 +8,33 @@ namespace Sat.Recruitment.Test.Controllers
 {
     [CollectionDefinition("Tests", DisableParallelization = true)]
     public class UsersControllerUnitTest
-    {        
+    {
+        private readonly UsersController userController;
+
+        /// <summary>
+        /// Constructur is called for every test
+        /// </summary>
+        public UsersControllerUnitTest()
+        {
+            var userRepository = new UsersService();
+            var userCreationService = new UserCreationService(userRepository);
+            var userValidationService = new UserValidationService();
+            
+            userController = new UsersController(userCreationService, userValidationService);
+        } 
+
+        /// <summary>
+        /// Do some cleanup for every test
+        /// </summary>
+        public void Dispose()
+        {
+
+        }
+
+
         [Fact]
         public async Task CreateUser_Normal_GivenAnyNewUser_OkExpected()
-        {
-            var userController = GetUserController();
-
+        {          
             var result = await userController.CreateUser("Normal 1", "normal1@gmail.com", "Address Normal 1", "+34111111111", "Normal", "111");
 
             Assert.True(result.IsSuccess);
@@ -23,8 +44,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_SuperUser_GivenAnyNewUser_OkExpected()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("SuperUser 1", "superuser1@gmail.com", "Address SuperUser 1", "+34222222222", "Normal", "222");
 
             Assert.True(result.IsSuccess);
@@ -34,8 +53,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_Premium_GivenAnyNewUser_OkExpected()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Premium 1", "premium1@gmail.com", "Address Premium 1", "+34333333333", "Premium", "333");
 
             Assert.True(result.IsSuccess);
@@ -45,8 +62,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_GivenAnyAlreadyExistingUserByEmail_UserDuplicatedErrorExpected()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Normal 1", "Juan@marmol.com", "Address Normal 1", "+34111111111", "Normal", "111");
 
             Assert.False(result.IsSuccess);
@@ -56,8 +71,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_GivenAnyAlreadyExistingUserByPhone_UserDuplicatedErrorExpected()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Normal 1", "normal1@gmail.com", "Address Normal 1", "+5491154762312", "Normal", "111");
 
             Assert.False(result.IsSuccess);
@@ -66,9 +79,7 @@ namespace Sat.Recruitment.Test.Controllers
 
         [Fact]
         public async Task CreateUser_GivenAnyAlreadyExistingUserByNameAddress_UserDuplicatedErrorExpected()
-        {
-            var userController = GetUserController();
-
+        {          
             var result = await userController.CreateUser("Juan", "Juan@marmol.com", "Peru 2464", "+34111111111", "Normal", "111");
 
             Assert.False(result.IsSuccess);
@@ -78,8 +89,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_CheckUserFieldsValidation_ErrorExpected()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser(null, null, null, null, "UserTypeInIncorrect", "NonParseableMoney");
 
             Assert.False(result.IsSuccess);
@@ -94,8 +103,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_EmailIncorrectFormat_EmailFormatErrorException()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Juan", "emailIncorrectFormat", "Peru 2464", "+5491154762312", "Normal", "1234");
 
             Assert.False(result.IsSuccess);
@@ -105,8 +112,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_EmailNull_EmailFormatException()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Juan", null, "Peru 2464", "+5491154762312", "Normal", "1234");
 
             Assert.False(result.IsSuccess);
@@ -116,8 +121,6 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_EmailEmpty_EmailFormatException()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Juan", string.Empty, "Peru 2464", "+5491154762312", "Normal", "1234");
 
             Assert.False(result.IsSuccess);
@@ -127,26 +130,10 @@ namespace Sat.Recruitment.Test.Controllers
         [Fact]
         public async Task CreateUser_UserTypeInIncorrect_UserTypeInIncorrectErrorException()
         {
-            var userController = GetUserController();
-
             var result = await userController.CreateUser("Juan", "Juan@marmol.com", "Peru 2464", "+5491154762312", "serTypeInIncorrect", "1234");
 
             Assert.False(result.IsSuccess);
             Assert.Contains("The userType is incorrect", result.Errors);
         }
-
-        #region "Private methods" 
-
-        private static UsersController GetUserController()
-        {
-            var userRepository = new UsersService();
-            var userCreationService = new UserCreationService(userRepository);
-            var userValidationService = new UserValidationService();
-            var userController = new UsersController(userCreationService, userValidationService);
-            
-            return userController;
-        }
-
-        #endregion
     }
 }
